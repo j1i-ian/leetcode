c400cd30f7c2d91d96149b607aadaf0dcf8e405e
Alan Lee <pdh2ndtt@duck.com>
2024-08-04 00:43:33 +0900

    Added mocha bootload

---
+++ scripts/mocha-bootload.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import * as chai from 'chai';
+
+import * as chaiAsPromised from 'chai-as-promised';
+
+import * as sinon from 'sinon';
+
+chai.use(chaiAsPromised);
+
+(globalThis as any).sinon = sinon;
